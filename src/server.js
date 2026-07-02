b534fdad8a4009abb48603fc074d28a175abd119
Yulia Fig <fihlevska@gmail.com>
2026-07-02 21:17:52 +0200

    1-hw-node

--- src/server.js
+++ src/server.js
@@ -9,9 +9,10 @@ import 'dotenv/config';
 
 // Решта коду
 const app = express();
-
-app.use(express.json());
+// Middleware
+app.use(express.json()); // Дозволяє роботу з файлами json
 app.use(cors()); // Дозволяє запити з будь-яких джерел
+// Дозволяє відстежувати як працює застосунок: які запити надходять
 app.use(
   pino({
     level: 'info',
@@ -31,18 +32,30 @@ app.use(
 // Використовуємо значення з .env або дефолтний порт 3000
 const PORT = process.env.PORT ?? 3000;
 
-// Перший маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+	message: `Retrieved all notes`
+}
+);
 });
 
-app.get('/user', (req, res) => {
-  res.status(200).json({ message: 'Hello user!' });
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+	message: `Retrieved note with ID: ${noteId}`
+}
+);
+});
+// Маршрут для тестування middleware помилки
+app.get('/test-error', (req, res) => {
+  // Штучна помилка для прикладу
+  throw new Error('Simulated server error');
 });
 
-app.get('/user/:userId/:name/:age', (req, res) => {
-  const { userId, name, age } = req.params;
-  res.status(200).json({ id: userId, name: name, age: age});
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
 });
 
 // Middleware для обробки помилок
